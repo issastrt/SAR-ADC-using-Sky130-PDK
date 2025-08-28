dacdb2e2b1297d1d91e679e34ccc2d11fd5ce85e
Samantha Daiz <samanthadaiz03@gmail.com>
2025-08-28 22:26:27 +0800

    updated cace

--- cace/scripts/run_project_lvs.tcl
+++ cace/scripts/run_project_lvs.tcl
@@ -13,9 +13,9 @@ set reflibs ${pdklib}/libs.ref
 set setupfile ${techlibs}/netgen/sky130A_setup.tcl
 set hvlib ${reflibs}/sky130_fd_sc_hvl/spice/sky130_fd_sc_hvl.spice
 
-set circuit1 [readnet spice $CACE_ROOT/netlist/layout/sky130_ef_ip__rdac3v_8bit.spice]
+set circuit1 [readnet spice $CACE_ROOT/netlist/layout/SAR-ADC-using-Sky130-PDK.spice]
 set circuit2 [readnet spice $hvlib]
-readnet spice $CACE_ROOT/netlist/schematic/sky130_ef_ip__rdac3v_8bit.spice $circuit2
+readnet spice $CACE_ROOT/netlist/schematic/SAR-ADC-using-Sky130-PDK.spice $circuit2
 
-lvs "$circuit1 sky130_ef_ip__rdac3v_8bit" "$circuit2 sky130_ef_ip__rdac3v_8bit" $setupfile \
-	sky130_ef_ip__rdac3v_8bit_comp.out -json
+lvs "$circuit1 SAR-ADC-using-Sky130-PDK" "$circuit2 SAR-ADC-using-Sky130-PDK" $setupfile \
+	SAR-ADC-using-Sky130-PDK_comp.out -json
